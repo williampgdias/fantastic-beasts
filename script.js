bb977fe0f3ff572f5c1df61a1a8669f931fbe769
William Dias <smdwilliam89@gmail.com>
2022-08-13 21:15:28 +0100

    Selecionando elemento 1

---
+++ script.js
@@ -0,0 +1,19 @@
+// getElementById
+const animals = document.getElementById('animals');
+console.log(animals);
+
+// getElementsByClassName
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+// querySelector
+const firstLi = document.querySelector('li');
+console.log(firstLi);
+
+// querySelector href
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
+
+// querySelectorAll
+const animalsImg = document.querySelectorAll('.animals img');
+console.log(animalsImg[1]);
