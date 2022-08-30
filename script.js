452d3dd3cd6a00eaf8585ca0c3b26119918db350
William Dias <smdwilliam89@gmail.com>
2022-08-30 15:54:34 +0100

    Animation Scroll

--- script.js
+++ script.js
@@ -69,3 +69,25 @@ const initSmoothScroll = () => {
   });
 };
 initSmoothScroll();
+
+// Scroll
+const initAnimationScroll = () => {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length) {
+    const halfWindow = window.innerHeight * 0.6;
+
+    const animationScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - halfWindow < 0;
+        if (isSectionVisible) section.classList.add('active');
+        else section.classList.remove('active');
+      });
+    };
+    animationScroll();
+
+    window.addEventListener('scroll', animationScroll);
+  }
+};
+initAnimationScroll();
